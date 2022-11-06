7f54914fdb1d033d586aae41124c28754e0753a6
akorede12 <akoredeaboaba12@gmail.com>
2022-11-05 22:12:42 -0700

    stage 2 edits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import ME from './me.jpg';
 import './App.css';
+import {Link} from 'react-router';
 
 function App() {
   return (
-    <div className="profile">
-        <div className="grid-container2">
-        <div className="grid-item2">
-        <img src={ME} className="mypicture" id='profile__img' alt="ME" />
-        <h1> Aboaba Akorede</h1>
+    <div className="App">
+        <div className="myImage">
+        <img src={ME} className="myImage" id='profile__img' alt="ME" />
         </div>
+
+        <h1 className='name'> Aboaba Akorede</h1>
+
+        <div className='twitterBox'>
+        <h2 id='twitter' className="twitter"> @Tame_nation</h2>
+        </div>
+
         <div>
-        <h2 id='twitter' className="grid-item2"> @Tame_nation</h2>
         <h2 id='slack' className="slack">AK</h2>
         </div>
+
+        <div className='zuriTeamBox'>
+          <a href='https://training.zuri.team/' id='btn__zuri' className='zuriTeam' > Zuri Team</a>
         </div>
-        <div className='grid-container'>
-          <a href='https://training.zuri.team/' id='btn__zuri' className='grid-item' > Zuri Team</a>
-          <a href='http://books.zuri.team' id='btn__zuri' className='grid-item' > Zuri Books</a>
-          <a href='https://books.zuri.team/python-for-beginners?ref_id=AK' id='book__python' className='grid-item' > Python Books</a>
-          <a href='https://background.zuri.team' id='pitch' className='grid-item'> Background check for orders</a>
-          <a href='https://books.zuri.team/design-rules' id='book__design' className='grid-item'> Design books</a>
+
+        <div className='zuriBooksBox'>
+          <a href='http://books.zuri.team' id='btn__zuri' className='zuriBooks' > Zuri Books</a>
+        </div>
+
+        <div className='pythonBooksBox'>
+          <a href='https://books.zuri.team/python-for-beginners?ref_id=AK' id='items' className='pythonBooks' > Python Books</a>
+        </div>
+
+        <div className='bcoBox'>
+          <a href='https://background.zuri.team' id='pitch' className='bco'> Background check for orders</a>
+        </div>
+
+        <div className='dbBox'>
+          <a href='https://books.zuri.team/design-rules' id='book__design' className='dB'> Design books</a>
+        </div>
+
+        <div className='contactBox'>
+          <Link href='/contact' id='contact' className='contact'> Contact</Link>
         </div> 
       </div>
   );
---
+++ src/contact.js
@@ -0,0 +1,66 @@
+// firstName, lastName, email, message 
+
+import { useState } from "react"
+
+function Contact(){
+    const[ userInput, setUserInput] = useState({
+        firstName: "", 
+        lastName: "",
+        Email: "",
+        Message: ""
+        })
+
+    const handleSubmit = () => {
+        alert("form was submitted")
+    }
+    
+    return(
+        <div>
+        <h1> Contact Me </h1>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+        <form>
+        <label> first name
+            <input
+            type="text"
+            placeholder="Enter your first name"
+            onChange={ e => setUserInput({...userInput, firstName: e.target.value}) }
+
+            />
+        </label>
+        <label> last name
+            <input
+            type="text"
+            placeholder= "Enter your last name"
+            onChange={ e => setUserInput({...userInput, lastName: e.target.value}) }
+
+            />
+        </label>
+        <label> Email
+            <input
+            type="text" 
+            placeholder= "yourname@email.com"
+            onChange={ e => setUserInput({...userInput, Email: e.target.value})}
+
+            />
+        </label>
+        <label> Message
+            <textarea
+            type="text"
+            placeholder= "Send me a message and I'll reply you as soon as possible..."
+            onChange={ e => setUserInput({...userInput, Message: e.target.value})}
+
+            />
+        </label>
+        <label> You agree to providing your data to name who may contact you.
+            <input type="radio" value = "checkbox" />
+        </label>
+        </form>
+
+        <button onClick={handleSubmit}>
+            Send message
+        </button>
+        </div>
+    )
+}
+
+export default Contact;
